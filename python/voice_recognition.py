123565d1e62eb3b84b402c192078355dc36eab9a
siantung <79162078+siantung@users.noreply.github.com>
2021-03-18 01:34:26 -0400

    Update voice_recognition.py

--- python/voice_recognition.py
+++ python/voice_recognition.py
@@ -1,11 +1,11 @@
 # sudo pip3 install SpeechRecognition
 # sudo apt-get install portaudio19-dev python-all-dev python3-all-dev && sudo pip3 install pyaudio
 
-import speech_Recognition as sr
+import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone as source:
+with sr.Microphone() as source:
     print('Enter Your Speech:')
     r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
