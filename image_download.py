78862306d471a6e256629058cb875ddbeaa69619
joeszym <79161911+joeszym@users.noreply.github.com>
2021-03-23 12:37:35 -0400

    Update image_download.py

--- image_download.py
+++ image_download.py
@@ -2,11 +2,10 @@ import urllib.request
 import os
 
 def download_web_image(url):
-    file_name = name + ".jpg"
-    full_name = os.path.join(path, file_name)
-    urllib.request.urlretrieve(url, full_name)
+    file_name = os.path.join(path, name)
+    urllib.request.urlretrieve(url, file_name)
 
 path = "/home/jszymkie/Desktop/PYPRO/images"
-name = "gil"
-url = "https://github.com/joeszym/ENEE_408I_Spring2021_images/blob/main/" + name + ".jpg?raw=true"
+name = "gil.jpg"
+url = "https://github.com/UMD-ENEE408I/ENEE408I_Spring_2021_Team_5/blob/main/Image/" + name + "?raw=true"
 download_web_image(url)
