49188b28e26bf057c3d209ea00a7b4b9828caea2
joeszym <79161911+joeszym@users.noreply.github.com>
2021-04-06 21:07:39 -0400

    Update images.py

--- python/images.py
+++ python/images.py
@@ -1,9 +1,9 @@
 import os
 
 File = "Levi.jpg"
-os.chdir("/home/jszymkie/ENEE408I_Spring_2021_Team_5/Image")
-os.system("git add " + File)
-os.system("git commit --no-verify --allow-empty-message " + File)
-#commit message still shows up :(
-os.system("git push --set-upstream " + File + " master")
-#This is untested because of commit
+os.chdir("/home/jszymkie/ENEE408I_Spring_2021_Team_5")
+os.system("git status")
+os.system("git add Image/" + File)
+os.system("git status")
+os.system("git commit -m image")
+os.system("git push")
