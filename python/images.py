c1133598abbfb205e48e1d26f09981a51035de11
amwhit24 <79162186+amwhit24@users.noreply.github.com>
2021-04-06 21:47:04 -0400

    Update images.py

--- python/images.py
+++ python/images.py
@@ -1,10 +1,10 @@
 import os
 
-File = "Levi.jpg"
-os.chdir("/home/jszymkie/ENEE408I_Spring_2021_Team_5")
+File = "(file name)"
+os.chdir("/home/(username)/ENEE408I_Spring_2021_Team_5")
 os.system("git status")
 os.system("git add Image/" + File)
 os.system("git status")
 os.system("git commit -m image")
 os.system("git push")
-os.system("git pull")
+os.system("git push")
