d9fe91124b895b1fe9dc008007712600621bd8f3
amwhit24 <austin10whitney24@gmail.com>
2021-04-06 20:46:58 -0400

    image

---
+++ OnlineComunication.py
@@ -0,0 +1,6 @@
+import os
+os.chdir("/home/amwhit24/ENEE408I_Spring_2021_Team_5")
+os.system("git status")
+os.system("git add OnlineComunication.py")
+os.system("git commit -m image")
+os.system("git push")
