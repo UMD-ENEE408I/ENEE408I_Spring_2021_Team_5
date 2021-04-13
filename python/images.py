b40d99628a1fb5d98788080aea0825c7d9415a5e
joeszym <79161911+joeszym@users.noreply.github.com>
2021-04-13 18:57:46 -0400

    Update images.py

--- python/images.py
+++ python/images.py
@@ -1,11 +1,7 @@
 import os
 
 os.system("git status")
-<<<<<<< HEAD
 os.system("git add Image")
-=======
-os.system("git add ../Image")
->>>>>>> 92350ccd0473e53c383ec5a76d1642c4a5a17e5c
 os.system("git status")
 os.system("git commit -m image")
 os.system("git push")
