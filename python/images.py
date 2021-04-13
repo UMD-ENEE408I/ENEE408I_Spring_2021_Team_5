6844a79f88503e2b5b652383476f93b5b11ffc27
Joey <joeszym12@gmial.com>
2021-04-13 18:19:47 -0400

    code

--- python/images.py
+++ python/images.py
@@ -1,10 +1,7 @@
 import os
 
-os.system("git pull")
-File = "(file name)"
-os.chdir("/home/(username)/ENEE408I_Spring_2021_Team_5")
 os.system("git status")
-os.system("git add Image/" + File)
+os.system("git add Image")
 os.system("git status")
 os.system("git commit -m image")
 os.system("git push")
