868733b266324efddabde8e6ded978fa1af6420c
siantung <79162078+siantung@users.noreply.github.com>
2021-04-14 00:56:40 -0400

    Update face10.py

--- python/face10.py
+++ python/face10.py
@@ -24,8 +24,8 @@ with open('train.pkl', 'rb') as f:
     Names = pickle.load(f)
     Encodings = pickle.load(f)
     
-faceFound = True
-unknownfaceFound = True
+# faceFound = True
+# unknownfaceFound = True
 
 
 camSet0 = 'nvarguscamerasrc sensor-id=1 ee-mode=2 ee-strength=0 tnr-mode=2 tnr-strength=1 wbmode=3 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1,format=NV12 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(width)+', height='+str(height)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! videobalance contrast=1.5 saturation=1.2 ! appsink drop=True'
@@ -50,9 +50,21 @@ while True:
         name1 = 'Unknown'
         matches = face_recognition.compare_faces(Encodings, face_encoding0)
         if True in matches:
-        unknownfaceFound = False
+        #unknownfaceFound = False
             first_match_index = matches.index(True)
             name1 = Names[first_match_index]
+            objX = left+right/2
+            errorX = objX - width/2
+            if abs(errorX)>left:
+                ser.write(b'l')
+            if abs(errorTilt0)> right:
+                ser.write(b'r')
+            if abs(errorX)<15:
+                ser.write(b'f')
+            if abs(errorX)>20:
+                ser.write(b'b')
+                time.sleep(1)
+                ser.write(b's')
             
         top = int(top/scaleFactor)
         right = int(right/scaleFactor)
@@ -60,22 +72,23 @@ while True:
         left = int(left/scaleFactor)
         cv2.rectangle(frame0,(left, top),(right, bottom), (255,0,0), 2)
         cv2.putText(frame0, name1, (left, top+0), font, .75, (0,0,255), 2 )
-    if faceFound = True:
-        objX = (left+right)/2
-        errorX = objX - width/2
-        if abs(errorX)>left:
-            ser.write(b'l')
-        if abs(errorTilt0)> right:
-            ser.write(b'r')
-        if abs(errorX)<15:
-            ser.write(b'f')
-        if abs(errorX)>20:
-            ser.write(b'b')
-            time.sleep(1)
-            ser.write(b's')
-        break
+        '''
+        if faceFound == True:
+            objX = left+right/2
+            errorX = objX - width/2
+            if abs(errorX)>left:
+                ser.write(b'l')
+            if abs(errorTilt0)> right:
+                ser.write(b'r')
+            if abs(errorX)<15:
+                ser.write(b'f')
+            if abs(errorX)>20:
+                ser.write(b'b')
+                time.sleep(1)
+                ser.write(b's')
+        '''    
 
-
+    
     for (top, right, bottom, left), face_encoding1 in zip(facePositions1, allEncodings1):
         name2 = 'Unknown'
         matches = face_recognition.compare_faces(Encodings, face_encoding1)
@@ -91,10 +104,13 @@ while True:
         cv2.putText(frame1, name2, (left, top+0), font, .75, (0,0,255),2 )
     
     frame3 = np.hstack((frame0, frame1))
+    
     dt = time.time()-timeMark
     timeMark = time.time()
+    
     dtFIL = 0.9*dtFIL + 0.1*dt       # Low-pass filter
     fps = 1/dtFIL
+    
     ### To show fps on the background      redcolor  solid box
     cv2.rectangle(frame3,(0,0), (150,40), (0,0,255), -1)
     cv2.putText(frame3, 'fps: '+str(round(fps,1)), (0, 30), font, 1, (0, 255, 255), 2)
@@ -102,8 +118,9 @@ while True:
     
     cv2.imshow('both_Cam',frame3)
     cv2.moveWindow('both_Cam',200,0)
-	
+
     if cv2.waitKey(1) == ord('q'):
         break
-both_Cam.release()
+	
+cam0.release()
 cv2.destroyAllWindows()
