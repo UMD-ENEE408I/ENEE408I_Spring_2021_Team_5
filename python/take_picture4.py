c96bc7379769436c5b5daec0ce1f208f8c3eaaa1
siantung <79162078+siantung@users.noreply.github.com>
2021-04-26 13:03:15 -0400

    Update take_picture4.py

--- python/take_picture4.py
+++ python/take_picture4.py
@@ -48,5 +48,38 @@ def selfie():
             break
     cam1.release()
     cv2.destroyAllWindows() 
+	
+Encodings = []
+Names = []
+image_dir = '/home/lian/Desktop/pyPro/images/known'
+
+
+def trainName():
+    def record_images(file):
+        fullPath = os.path.join(root,file)
+        print(fullPath)
+        name = os.path.splitext(file)[0]
+        print(name)
+        person = face_recognition.load_image_file(fullPath)
+        encoding = face_recognition.face_encodings(person)[0]
+        Encodings.append(encoding)
+        Names.append(name)
+    print(Names)
+    with open('train.pkl', 'wb') as f:
+        pickle.dump(Names, f)
+        pickle.dump(Encodings, f)
+        
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(record_images, files)
+
+
+threads = []
+for root, dirs, files in os.walk(image_dir):
+    t = threading.Thread(target=trainName)
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
 
       
