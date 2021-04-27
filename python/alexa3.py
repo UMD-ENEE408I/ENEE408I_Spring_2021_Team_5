6634ac171f7289ecb9539b1cf1c7016128eb8679
siantung <79162078+siantung@users.noreply.github.com>
2021-04-27 11:50:23 -0400

    Update alexa3.py

--- python/alexa3.py
+++ python/alexa3.py
@@ -29,7 +29,7 @@ def wander():
 def go_forward():
     ser.write(b'f')
     speech_text = 'Going forward'
-    return statement(speech_text)#.simple_card('My Robot', speech_text)
+    return statement(speech_text).simple_card('My Robot', speech_text)
 
 @ask.intent('Backward')
 def go_forward():
