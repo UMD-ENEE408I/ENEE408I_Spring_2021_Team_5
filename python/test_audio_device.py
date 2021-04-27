6020eccca89ab9aea48714d3e60c12111b3c6347
siantung <79162078+siantung@users.noreply.github.com>
2021-04-27 12:09:17 -0400

    Delete test_audio_device.py

--- python/test_audio_device.py
+++
@@ -1,3 +0,0 @@
-import speech_recognition as sr
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Names: \"{0}\" found for'Microphone(device_index={1})'".format(index, name))
