19262385f244cd716eae1ca910d99ae5ce26b566
siantung <79162078+siantung@users.noreply.github.com>
2021-05-01 05:36:37 -0400

    Update face10.py

--- python/face10.py
+++ python/face10.py
@@ -11,8 +11,7 @@ ser = serial.Serial('/dev/ttyUSB0')
 print(cv2.__version__)
 timeMark = time.time()
 dtFIL = 0
-scaleFactor = .8
-#a = 100
+scaleFactor = .4
 
 width = 720
 height = 480
@@ -25,10 +24,6 @@ with open('train.pkl', 'rb') as f:
     Names = pickle.load(f)
     Encodings = pickle.load(f)
     
-# faceFound = True
-# unknownfaceFound = True
-
-
 camSet0 = 'nvarguscamerasrc sensor-id=1 ee-mode=2 ee-strength=0 tnr-mode=2 tnr-strength=1 wbmode=3 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1,format=NV12 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(width)+', height='+str(height)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! videobalance contrast=1.5 saturation=1.2 ! appsink drop=True'
 camSet1 = 'nvarguscamerasrc sensor-id=0 ee-mode=2 ee-strength=0 tnr-mode=2 tnr-strength=1 wbmode=3 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1,format=NV12 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(width)+', height='+str(height)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! videobalance contrast=1.5 saturation=1.2 ! appsink drop=True'
 	
@@ -39,13 +34,13 @@ while True:
     _, frame0 = cam0.read()
     _, frame1 = cam1.read()
     frameSmall0 = cv2.resize(frame0,(0,0), fx=scaleFactor, fy=scaleFactor)#smaller scale will faster the detection but it needs bigger face to reconize it.
-    frameSmall1 = cv2.resize(frame1,(0,0), fx=scaleFactor, fy=scaleFactor)
+    #frameSmall1 = cv2.resize(frame1,(0,0), fx=scaleFactor, fy=scaleFactor)
 
     facePositions0 = face_recognition.face_locations(frameSmall0)
-    facePositions1 = face_recognition.face_locations(frameSmall1)
+    #facePositions1 = face_recognition.face_locations(frameSmall1)
     
     allEncodings0 = face_recognition.face_encodings(frameSmall0, facePositions0)
-    allEncodings1 = face_recognition.face_encodings(frameSmall1, facePositions1)
+    #allEncodings1 = face_recognition.face_encodings(frameSmall1, facePositions1)
     
     for (top, right, bottom, left), face_encoding0 in zip(facePositions0, allEncodings0):
         name1 = 'Unknown'
@@ -61,80 +56,26 @@ while True:
         cv2.rectangle(frame0,(left, top),(right, bottom), (255,0,0), 2)
         cv2.putText(frame0, name1, (left, top+0), font, .75, (0,0,255), 2 )
         
-        #area = cv2.rectangle(frame0,(left, top),(right, bottom), (255,0,0), 2)
-        try:
-            objY = (left+right)/2
-            errorY = objY - width/2
 
-            ser.write(b'f')
-            time.sleep(1)
-            if abs(errorY)>100:
-                ser.write(b'f')
-                
-                if abs(errorY)>left:
-                    ser.write(b'l')
-                    time.sleep(0.5)
-                    ser.write(b'f')
-                    time.sleep(1.5)
-                else:
-                    ser.write(b'r')
-                    time.sleep(0.5)
-                    ser.write(b'f')
-                    time.sleep(1.5)
-                
-            else:
-                    ser.write(b's')
-                    break
-            if False in matches:
-                ser.write(b's')
+        objY = abs(left+right)/2
+        errorY = objY - width/2
+
+        if abs(errorY)>50:              
+            if abs(errorY)>left:
+                ser.write(b'r')
+                print('llllllllllllllllll')
                 break
-        except:
+
+            else:
+                ser.write(b'l')
+                print('rrrrrrrrrrrrrrrrrrrr')
+                break
+                
+        else:
+            ser.write(b'f')
+            print('middleeeeeeeeeeeeeeeee')
             break
         
-
-    for (top, right, bottom, left), face_encoding1 in zip(facePositions1, allEncodings1):
-        name2 = 'Unknown'
-        matches = face_recognition.compare_faces(Encodings, face_encoding1)
-        if True in matches:
-            first_match_index = matches.index(True)
-            name2 = Names[first_match_index]
-
-        top = int(top/scaleFactor)
-        right = int(right/scaleFactor)
-        bottom = int(bottom/scaleFactor)
-        left = int(left/scaleFactor)
-        cv2.rectangle(frame1,(left, top),(right, bottom), (255,0,0), 2)
-        cv2.putText(frame1, name2, (left, top+0), font, .75, (0,0,255),2 )
-        '''
-        try:
-            #objX = left/2 
-            objY = (left+right)/2
-            #errorX = objX - width/2
-            errorY = objY - width/2
-            ser.write(b'f')
-            if abs(errorY)>100:
-                ser.write(b'f')
-                
-                if abs(errorY)>left:
-                    ser.write(b'l')
-                    time.sleep(0.5)
-                    ser.write(b'f')
-                    time.sleep(1.5)
-                else:
-                    ser.write(b'r')
-                    time.sleep(0.5)
-                    ser.write(b'f')
-                    time.sleep(1.5)
-                
-            else:
-                    ser.write(b's')
-                    break
-            if False in matches:
-                ser.write(b's')
-                break
-        except:
-            break
-'''
     frame3 = np.hstack((frame0, frame1))
     dt = time.time()-timeMark
     fps = 1/dt
@@ -149,6 +90,6 @@ while True:
 	
     if cv2.waitKey(1) == ord('q'):
         break
-	
-cam0.release()
+    
+both_Cam.release()
 cv2.destroyAllWindows()
