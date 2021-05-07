895f87086c7ef5082e6a7effafd75a6a17cfefc1
siantung <79162078+siantung@users.noreply.github.com>
2021-05-06 20:26:12 -0400

    Update face10.py

--- python/face10.py
+++ python/face10.py
@@ -7,11 +7,13 @@ import pickle
 import os
 import time
 import serial
+import threading
 ser = serial.Serial('/dev/ttyUSB0')
 print(cv2.__version__)
 timeMark = time.time()
 dtFIL = 0
-scaleFactor = .4
+scaleFactor = .5
+reset_Timer = 8
 
 width = 720
 height = 480
@@ -30,7 +32,19 @@ camSet1 = 'nvarguscamerasrc sensor-id=0 ee-mode=2 ee-strength=0 tnr-mode=2 tnr-s
 cam0 = cv2.VideoCapture(camSet0)
 cam1 = cv2.VideoCapture(camSet1)
 
-while True:
+def countdown():
+    global reset_Timer
+    while reset_Timer > 0:
+        print('T-minus', reset_Timer)
+        time.sleep(1)
+        reset_Timer -= 1
+        
+count = threading.Thread(target=countdown)
+count.start()
+
+
+while reset_Timer>0:
+    
     _, frame0 = cam0.read()
     _, frame1 = cam1.read()
     frameSmall0 = cv2.resize(frame0,(0,0), fx=scaleFactor, fy=scaleFactor)#smaller scale will faster the detection but it needs bigger face to reconize it.
@@ -44,32 +58,38 @@ while True:
     
     for (top, right, bottom, left), face_encoding0 in zip(facePositions0, allEncodings0):
         name1 = 'Unknown'
+        reset_Timer = 8
         matches = face_recognition.compare_faces(Encodings, face_encoding0)
         if True in matches:
             first_match_index = matches.index(True)
             name2 = Names[first_match_index]
                     
+        
         top = int(top/scaleFactor)
         right = int(right/scaleFactor)
         bottom = int(bottom/scaleFactor)
         left = int(left/scaleFactor)
         cv2.rectangle(frame0,(left, top),(right, bottom), (255,0,0), 2)
-        cv2.putText(frame0, name1, (left, top+0), font, .75, (0,0,255), 2 )
+        cv2.putText(frame0, name2, (left, top+0), font, .75, (0,0,255), 2 )
         
 
         objY = abs(left+right)/2
         errorY = objY - width/2
 
-        if abs(errorY)>50:              
-            if abs(errorY)>left:
-                ser.write(b'r')
-                print('llllllllllllllllll')
-                break
+        print(left)
+        print(right)
+        print(errorY)
+        print(name2)
+                     
+        if errorY<-50:
+            ser.write(b'l')
+            print('llllllllllllllllll')
+            break
 
-            else:
-                ser.write(b'l')
-                print('rrrrrrrrrrrrrrrrrrrr')
-                break
+        elif errorY>50:
+            ser.write(b'r')
+            print('rrrrrrrrrrrrrrrrrrrr')
+            break
                 
         else:
             ser.write(b'f')
