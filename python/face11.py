8b962e11ce8945f17718ccac104aeacf8a158843
amwhit24 <79162186+amwhit24@users.noreply.github.com>
2021-05-10 16:03:06 -0400

    Update face11.py

--- python/face11.py
+++ python/face11.py
@@ -24,7 +24,7 @@ def known_Greeting():
     ser = serial.Serial('/dev/ttyUSB0')
     timeMark = time.time()
     dtFIL = 0
-    scaleFactor = .5
+    scaleFactor = .8
 
     width = 720
     height = 480
